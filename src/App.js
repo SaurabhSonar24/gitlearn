606e23f2957567199137b34177b560b3c477b8a3
SaurabhSonar <sonarsaurabh1@gmail.com>
2022-12-05 12:11:43 +0530

    Second_branch first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import First_branch from './Components/First_branch'; 
+import Second_branch from './Components/Second_branch';
 
 function App() {
   return (
     <div>
       <First_branch/>
+      <Second_branch/>
     </div>
   );
 }
---
+++ src/Components/Second_branch.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Second_branch() {
+  return (
+    <div>Second_branch</div>
+  )
+}
