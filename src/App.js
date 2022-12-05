65079e0b873cf838ae9e80c17196d5a7e19ba2ad
SaurabhSonar <sonarsaurabh1@gmail.com>
2022-12-05 11:17:39 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import First_branch from './Components/First_branch'; 
 
 function App() {
   return (
     <div>
-      
+      <First_branch/>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/Components/First_branch.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function First_branch() {
+  return (
+    <div>First_branch</div>
+  )
+}
